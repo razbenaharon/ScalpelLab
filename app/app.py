46b71865bb1419294f23a696be6c0a3bc8a36a62
‪Fadi Mahameed‬‏ <155967856+Fadi-Mahameed@users.noreply.github.com>
2026-01-01 09:48:43 +0200

    Fix Streamlit app pages

--- app/app.py
+++ app/app.py
@@ -55,7 +55,7 @@ if os.path.exists(erd_pdf_path):
 
         # Convert to PIL Image and display
         image = Image.open(io.BytesIO(img_data))
-        st.image(image, caption="ScalpelLab Database Entity Relationship Diagram", use_container_width=True)
+        st.image(image, caption="ScalpelLab Database Entity Relationship Diagram", width='stretch')
 
         pdf_document.close()
 
